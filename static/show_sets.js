d4cb7462742a821fe04615e36478b5b6bc052162
David Lee <davidlee980804@gmail.com>
2020-12-23 15:41:10 -0800

    Rough draft of card flip

--- static/show_sets.js
+++ static/show_sets.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const $carousel = 
+const $carousel = $("#set-carousel");
 
 /** Make the first card active so that the 
  *  carousel will function properly
@@ -14,4 +14,19 @@ $(document).ready(makeFirstCardActive);
 
 
 
-$()
+
+
+function handleFlipCard(evt) {
+  let $cardBody = $(evt.target).closest(".card-body");
+
+  console.log($cardBody);
+
+  if ($cardBody.hasClass("flipped")) {
+    $cardBody.removeClass("flipped");
+  } else {
+    $cardBody.addClass("flipped");
+  }
+}
+
+
+$carousel.on("click", ".card-body", handleFlipCard);
