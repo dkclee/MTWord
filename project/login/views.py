e73c67be3410dc047c720328d7aca2f90b9d492d
David Lee <davidlee980804@gmail.com>
2021-01-13 19:11:35 -0800

    refactor code format

--- project/login/views.py
+++ project/login/views.py
@@ -2,8 +2,7 @@
 from flask import Blueprint, render_template, flash, redirect, url_for, \
     request, abort
 
-from flask_login import login_user, logout_user, current_user, \
-    login_manager
+from flask_login import login_user, logout_user, current_user
 
 from flask_mail import Message
 
